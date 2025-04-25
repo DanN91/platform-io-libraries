7bccae0ab7dfd17ae05a49c57429e5599e3bd8dd
DanN91 <nisdan91@gmail.com>
2025-04-25 13:45:53 +0300

    Library: use C++20 and non-const [] operator
    Use std::move instead of copying the item when assigning.

--- Array/src/Array.h
+++ Array/src/Array.h
@@ -19,9 +19,11 @@ public:
     bool Add(T item)
     {
         if (m_count == m_capacity)
+        {
             return false;
+        }
 
-        m_data[m_count] = item;
+        m_data[m_count] = std::move(item);
         m_count++;
 
         return true;
@@ -30,11 +32,16 @@ public:
     void Remove(uint32_t index)
     {
         if (index >= m_count)
+        {
             return;
+        }
 
         m_count--;
+
         for (uint32_t i = index; i < m_count; ++i)
+        {
             m_data[i] = m_data[i + 1]; // overwrite free slot and move it at the end for O(1) addition
+        }
     }
 
     void Clear()
@@ -47,10 +54,12 @@ public:
         return m_count;
     }
 
-    const T* operator[](uint32_t index) const
+    T* operator[](uint32_t index)
     {
         if (index >= m_count)
+        {
             return nullptr;
+        }
 
         return &m_data[index];
     }
